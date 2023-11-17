76aff86516e1d626f52286bd6ba9804742cbec2e
Luke Shafer <lukealanshafer@gmail.com>
2023-11-17 01:05:14 -0500

    Updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
-import { fontFamily } from "tailwindcss/defaultTheme"
+import { fontFamily } from 'tailwindcss/defaultTheme';
+import tailwindScrollbar from 'tailwind-scrollbar';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: [ "Poppins", ...fontFamily.sans ],
+				sans: ['Poppins', ...fontFamily.sans],
 			},
 		},
 	},
-	plugins: [],
+	plugins: [tailwindScrollbar],
 };
